c7b9cae7628c011b34210108f042df03b2323a1e
yakovlev-v <vm-win7@users.noreply.github.com>
2020-03-09 15:35:11 +0500

    Добавлены параметризованные методы

--- cat_test_testgroup.py
+++ cat_test_testgroup.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
+
 
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
@@ -16,32 +15,63 @@ class AppDynamicsJob(unittest.TestCase):
         self.base_url = "https://www.google.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_app_dynamics_job(self):
-        driver = self.driver
+
+    def open_home_page(self, driver):
         driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
+
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, driver, group):
+        # открыть страницу групп
         driver.find_element_by_name("new").click()
+        # ввод данных группы
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("test")
+        driver.find_element_by_name("group_header").send_keys(group .header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("test")
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        driver.find_element_by_xpath("//form[@action='/addressbook/group.php']").click()
         driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("groups").click()
+
+    def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
-    
+
+    def close_browser(self, driver):
+        driver.close()
+
+    def test_add_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="test", header="test", footer="test"))
+        self.open_groups_page(driver)
+        self.logout(driver)
+        self.close_browser(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.open_groups_page(driver)
+        self.logout(driver)
+        self.close_browser(driver)
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -67,6 +97,7 @@ class AppDynamicsJob(unittest.TestCase):
         # To know more about the difference between verify and assert,
         # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
