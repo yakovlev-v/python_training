45f4e88bcc0e287de937fbb39c76e38e1d0d852b
yakovlev-v <vm-win7@users.noreply.github.com>
2020-03-09 21:17:31 +0500

    созданы пакеты фикстур, тестов и модулей, файлы тестов перенесены в соответствующие пакеты

--- test/test_testgroup.py
+++ test/test_testgroup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture()
@@ -21,4 +21,3 @@ def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
